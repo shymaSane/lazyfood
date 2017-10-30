aff32ac97be6915332823402911d1cc4227477d1
shymaSane <shymaSane1@gmail.com>
2017-10-30 21:23:07 +0200

    shopping-list diplaying dommy data

--- quick/src/app/sharing/ingredients.model.ts
+++ quick/src/app/sharing/ingredients.model.ts
@@ -1,4 +1,4 @@
 export class Ingredients {
-constructor(public name: string, public amount: string){}
+constructor(public name: string, public amount: number){}
 }
 // this do the same work as recipes.model.ts its a shortened way to write it 
--- quick/src/app/shopping-list/shopping-list.component.ts
+++ quick/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredients} from '../sharing/ingredients.model'
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-ingredients = []
+ingredients: Ingredients[] = [
+  // here we are using the consructor to make new object thats why we made the model short 
+  // in both cases we ll use ngfor inside th anchor tag
+  new Ingredients ('tomato', 3),
+  new Ingredients ('garlic', 1),
+]
   constructor() { }
 
   ngOnInit() {
